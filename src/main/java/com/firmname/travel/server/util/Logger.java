823e1c0d9790b705446763a65b97871553745e22
GreatJason <registerpro@163.com>
2015-12-05 00:38:47 +0800

    fix bug: Logger can not find caller classname if nested calling happens

--- src/main/java/com/firmname/travel/server/util/Logger.java
+++ src/main/java/com/firmname/travel/server/util/Logger.java
@@ -14,11 +14,25 @@ public final class Logger {
 
 	//logger cache
 	private final static Map<String, org.slf4j.Logger> loggerCache = new ConcurrentHashMap<String, org.slf4j.Logger>();
+	private final static String loggerClassName;
 	
-	public static org.slf4j.Logger getLogger(){
+	static {
+		loggerClassName = new Object(){
+			public String getClassName() 
+			{
+				String innerClass = this.getClass().getName();
+				return innerClass.substring(0, innerClass.lastIndexOf('$'));
+			}
+		}.getClassName();
+	}
+	
+	private static org.slf4j.Logger getLogger(){
 		//TODO: revisit for synchronized
 		org.slf4j.Logger logger = null;
-		String currentClass =  Thread.currentThread().getStackTrace()[2].getClassName();
+		for(StackTraceElement ste:Thread.currentThread().getStackTrace()){
+			System.out.println(ste.getClassName());
+		}
+		String currentClass = findCallerClass();
 		if(loggerCache.containsKey(currentClass)){
 			logger = loggerCache.get(currentClass);
 		}else{
@@ -26,6 +40,16 @@ public final class Logger {
 			loggerCache.put(currentClass, logger);
 		}
 		return logger;
+	}
+	
+	private static String findCallerClass(){
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		for(int i = 1; i < ste.length; ++i){
+			if(!ste[i].getClassName().equals(loggerClassName)){
+				return ste[i].getClassName();
+			}
+		}
+		return loggerClassName;
 	}
 	
 	public static void trace(String format, Object... args) {
